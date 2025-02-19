a5148e214ee552cfc7914e161c1e3cd0f1a86b5e
Saksham Singh <155532123+Saksham1136@users.noreply.github.com>
2025-02-19 15:25:15 +0000

    ij

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/DALL·E-2025-02-19-20.33.jpeg";
+import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
